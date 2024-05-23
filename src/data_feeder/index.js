14271800eae1a885e2952c1af355a4366572888e
Michael Trauner <e12019868@student.tuwien.ac.at>
2024-05-24 00:07:51 +0200

    Datafeeder: Mock Control service, Extend vehicle behaviour

--- src/data_feeder/index.js
+++ src/data_feeder/index.js
@@ -1,24 +1,34 @@
 
 import dotenv from 'dotenv';
 import { FollowingVehicle, LeadingVehicle } from './vehicle.js';
+import {Simulator} from "./simulator.js";
 
 dotenv.config();
 
-const leading= new LeadingVehicle( 48.2303098, 16.4018315 );
-await leading.register();
-await leading.openChannel();
+const sim = new Simulator();
+sim.createVehicles(1);
+await sim.startVehicles();
+await sim.startMockControlService();
+await sim.startLVManeuvers();
+//sim.stopVehicles();
 
-leading.startUpdaterTimer();
+// const leading= new LeadingVehicle( 48.2303098, 16.4018315, 90, 45 );
+// await leading.register();
+// await leading.openChannel();
+//
+// leading.startUpdaterTimer();
+//
+// const following= new FollowingVehicle( 38.2303098, 16.4018315, 90 , 50 )
+// await following.register();
+// await following.openChannel();
+//
+// following.startUpdaterTimer();
 
-const following= new FollowingVehicle( 38.2303098, 16.4018315 )
-await following.register();
-await following.openChannel();
+//let inside= false;
 
-following.startUpdaterTimer();
-
-let inside= false;
-setInterval(() => {
-  following.long= inside ? 48.2303098 : 38.2303098;
-  inside= !inside;
-
-}, 7000);
+// Every 7 seconds the following vehicle jumps out of range/into range
+// setInterval(() => {
+//   following.long= inside ? 48.2303098 : 38.2303098;
+//   inside= !inside;
+//
+// }, 7000);
